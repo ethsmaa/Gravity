e12849e4e059993dfc463198f32cea3c81883fe9
ShodaiFatih <111004521+FatihBoz@users.noreply.github.com>
2023-04-05 19:14:43 +0300

    Update Gravity.java

--- src/Gravity.java
+++ src/Gravity.java
@@ -19,6 +19,10 @@ public class Gravity {
     // ----------------------------------------------------
 
 
+	Stack displayStack = new Stack(8);
+	Player player;
+	
+	
     Gravity() throws Exception {
         // --- Contructor
         // ------ Standard code for mouse and keyboard ------ Do not change
@@ -119,11 +123,36 @@ public class Gravity {
         }
         map[py][px] = 'P';
         cn.getTextWindow().output(px, py, 'P');
+        player = new Player(px,py);
 
         // print map
         printMap(map);
+        
+        for(int i = 0;i < 18;i++) {
+      	  if(i <9) {cn.getTextWindow().output(72,6+i,'|');}
+      	  else {cn.getTextWindow().output(76,i-3,'|');}
+        }
+        for(int i = 0;i < 5;i++) {
+      	  if(i == 0 || i == 4) {cn.getTextWindow().output(72+i,14,'+');}
+      	  else {cn.getTextWindow().output(72+i,14,'-');}
+        }
+        
+        cn.getTextWindow().setCursorPosition(71, 15);
+  		cn.getTextWindow().output("Backpack");
+  		
+  		cn.getTextWindow().setCursorPosition(68, 18);
+  		cn.getTextWindow().output("Teleport :   3");
+
+  	
+  		cn.getTextWindow().setCursorPosition(68, 20);
+  		cn.getTextWindow().output("Score    :   0");
+  	
+  		cn.getTextWindow().setCursorPosition(68, 22);
+  		cn.getTextWindow().output("Time     :   0");
+        
 
         while (true) {
+        	
             if (mousepr == 1) {  // if mouse button pressed
                 cn.getTextWindow().output(mousex, mousey, '#');  // write a char to x,y position without changing cursor position
                 px = mousex;
@@ -148,19 +177,43 @@ public class Gravity {
                     cn.getTextWindow().output(px, py, ' ');
                     py++;
                 }
+                
 
-                char rckey = (char) rkey;
-                //        left          right          up            down
-                if (rckey == '%' || rckey == '\'' || rckey == '&' || rckey == '(')
-                    cn.getTextWindow().output(px, py, 'P'); // VK kullanmadan test teknigi
-                else cn.getTextWindow().output(rckey);
+                
+                
+            if(map[py][px] == '1' || map[py][px] == '2' || map[py][px] == '3') {
+            	if(map[py][px] == '1') {player.addNewItem("1");}
+                else if(map[py][px] == '2') {player.addNewItem("2");}
+                else if (map[py][px] == '3') {player.addNewItem("3");}
+                
+          		displayBackpack();
+          		
+                cn.getTextWindow().setCursorPosition(68, 8);
+          		cn.getTextWindow().output("Teleport :   "+player.getTpRights());
 
-                if (rkey == KeyEvent.VK_SPACE) {
-                    String str;
-                    str = cn.readLine();     // keyboardlistener running and readline input by using enter
-                    cn.getTextWindow().setCursorPosition(5, 20);
-                    cn.getTextWindow().output(str);
-                }
+          	
+          		cn.getTextWindow().setCursorPosition(68, 10);
+          		cn.getTextWindow().output("Score    :   "+player.getScore());
+          	
+          		cn.getTextWindow().setCursorPosition(68, 12);
+          		cn.getTextWindow().output("Time     :   0");//getTime()
+            }
+            map[py][px] = 'P';
+            cn.getTextWindow().output(px, py, 'P');
+            
+            char rckey = (char) rkey;
+            //        left          right          up            down
+            if (rckey == '%' || rckey == '\'' || rckey == '&' || rckey == '(')
+                cn.getTextWindow().output(px, py, 'P'); // VK kullanmadan test teknigi
+            else cn.getTextWindow().output(rckey);
+
+            if (rkey == KeyEvent.VK_SPACE) {
+                String str;
+                str = cn.readLine();     // keyboardlistener running and readline input by using enter
+                cn.getTextWindow().setCursorPosition(5, 20);
+                cn.getTextWindow().output(str);
+            }
+
 
                 keypr = 0;    // last action
             }
@@ -182,6 +235,17 @@ public class Gravity {
                 cn.getTextWindow().output(j, i, map[i][j]);
             }
         }
+    }
+    
+    void displayBackpack() {
+ 	   while(!player.getBackpack().isEmpty()) {displayStack.push(player.getBackpack().pop());}
+ 	   int a = 0;
+ 	   while(!displayStack.isEmpty()) {
+ 		   char c = ((String) displayStack.peek()).charAt(0);
+ 		   player.getBackpack().push(displayStack.pop());
+ 		   cn.getTextWindow().output(74,13-a,c);
+ 		   a++;
+ 	   }
     }
 
 }
